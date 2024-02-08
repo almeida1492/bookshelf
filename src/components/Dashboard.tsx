3b27e40fdefb290d268bb2da6fc4e3e8adc3791b
hribeiro <henrique.ribeiro@intesys.it>
2024-02-08 09:05:08 +0100

    Fetch and display books

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { StateContext } from "../App";
 
 export function Dashboard() {
   const { username } = useContext(StateContext);
-  return <div>Welcome {username}</div>;
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://freetestapi.com/api/v1/books")
+      .then((result) => result.json())
+      .then((obj) => setData(obj));
+  }, []);
+
+  return (
+    <div className="bookshelf">
+      {data.map((item) => (
+        <div className="book">
+          <img className="cover" src={item.cover_image} height={100} />
+          <div>
+            <b>Titolo:</b> {item.title}
+          </div>
+          <div>
+            <b>Autore:</b> {item.author}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
